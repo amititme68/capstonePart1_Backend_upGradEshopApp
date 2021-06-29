bd0efcd76891ebb95f6c15332e80cbaa506897df
amititme68 <amititme68@gmail.com>
2021-06-30 01:17:45 +0530

    Implemented API for SIGNUP of users

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const products = require('./routes/products');
-const addresses = require('./routes/addresses');
+const products = require("./routes/products");
+const addresses = require("./routes/addresses");
+const orders = require("./routes/orders");
+const users = require("./routes/users");
 const mongoose = require("mongoose");
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useUnifiedTopology", true);
@@ -11,12 +13,13 @@ mongoose
   })
   .catch((err) => console.error("Couldnot connected to database"));
 
-
- const express = require("express");
- const app = express();
+const express = require("express");
+const app = express();
 app.use(express.json());
-app.use('/products', products);
-app.use('/addresses', addresses);
+app.use("/products", products);
+app.use("/addresses", addresses);
+app.use("/orders", orders);
+app.use("/users", users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
