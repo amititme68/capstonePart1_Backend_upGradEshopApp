91a6868436b8d5c1aea7c731c6e060db82d808e9
amititme68 <amititme68@gmail.com>
2021-07-01 00:11:14 +0530

    Implemented authentication API and created valid JSON web token signed by private key

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+const config = require("config");
 const products = require("./routes/products");
+const auth = require("./routes/auth");
 const addresses = require("./routes/addresses");
 const orders = require("./routes/orders");
 const users = require("./routes/users");
 const mongoose = require("mongoose");
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useUnifiedTopology", true);
+
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined");
+  process.exit(1); // 0 means success anything else means failure, so if jwtPrivateKey is not set we get error and process exit.
+}
 
 mongoose
   .connect("mongodb://localhost/upGrad_Eshop_application")
@@ -20,6 +27,7 @@ app.use("/products", products);
 app.use("/addresses", addresses);
 app.use("/orders", orders);
 app.use("/users", users);
+app.use("/auth", auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
